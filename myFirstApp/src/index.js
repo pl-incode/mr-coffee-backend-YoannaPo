46296eab62d9aea633a903f96b42894e759c003a
YoannaPo <yoannna.pol@gmail.com>
2022-02-28 23:19:20 +0000

    Add user Id support

--- myFirstApp/src/index.js
+++ myFirstApp/src/index.js
@@ -2,8 +2,17 @@
 // 1.import express
 const express = require("express");
 
+// umozliwia pobranie content type application json
+const bodyParser = require("body-parser");
+
+const myData = require("./data");
+
+
+
 // 2.inicjalizacja
 const app = express();
+//Expressie uzywaj bodyParser
+app.use(bodyParser.json());
 
 // 3.zdefiniowanie handlera 
 app.get("/", (req, res) => {
@@ -18,7 +27,17 @@ app.get("/schedules", (req, res) => {
   res.send(myData.schedules); //zwraca liste terminow
 });
 
-const myData = require("./data");
+app.get("/users/:id", (req, res) => {
+  const idNumber = req.params.id; 
+  if (idNumber >= myData.users.length){
+    res.json("No such a user");
+    return;}
+  res.json(myData.users[idNumber]);
+});
+
+
+
+//const myData = require("./data");
 
 // 4.ustawienie nasluchiwania
 
